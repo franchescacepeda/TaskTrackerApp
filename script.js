62bd2a84f8cbc4445e47a0eb950b8aa8c5719dba
Franchesca Cepeda <31700834+franchescacepeda@users.noreply.github.com>
2024-03-13 21:45:10 -0400

    Implement task rendering

--- script.js
+++ script.js
@@ -24,35 +24,12 @@ function handleSubmission(event){
     }
     
 }   
-var newTask = { name: taskName, description: taskDescription, deadline: taskDeadline};
-tasks.push(newTask);
-render();
-function render(){
-    taskTable.innerHTML= '';
-    tasks.forEach(function(task){
-        var row = document.createElement('tr');
-
-        const nameCell = document.createElement('td');
-        nameCell.textContent = task.name;
-        row.appendChild(nameCell);
-
-        const descriptionCell = document.createElement('td');
-        descriptionCell.textContent = task.description;
-        row.appendChild(descriptionCell);
-
-        const deadlineCell = document.createElement('td');
-        deadlineCell.textContent = task.deadline;
-        row.appendChild(deadlineCell);
-
-        taskTable.appendChild(row);
-    });
-
-
-    }
-
-    function init(){
-        render();
-
-    }
-    taskForm.addEventListener("submit", handleSubmission);
-    init();
+taskTable.innerHTML = tasks.map(task => `
+    <tr>
+        <td>${task.name}</td>
+        <td>${task.description}</td>
+        <td>${task.deadline}</td>
+        <td><button onclick="markTaskComplete(this)">Complete</button></td>
+        <td><button onclick="removeTask(this)">Remove</button></td>
+    </tr>
+`).join('');
