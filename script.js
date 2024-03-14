23b143a3d7f5993339ab51cd702007442c5a5e78
Franchesca Cepeda <31700834+franchescacepeda@users.noreply.github.com>
2024-03-13 21:42:25 -0400

    Implement form submission handling

--- script.js
+++ script.js
@@ -3,3 +3,56 @@ const taskForm =document.getElementById("taskForm");
 const taskTable = document.getElementById("taskTable");
 console.log(taskForm);
 console.log(taskTable);
+
+function handleSubmission(event){
+    event.preventDefault();
+    const taskName = document.getElementById("taskName").value;
+    const taskDescription = document.getElementById("taskDescription").value;
+    const taskDeadline= document.getElementById("taskDeadline").value;
+
+    if(taskName.trim() === ''){
+        alert("Enter a task name.");
+        return;
+    }
+    if(taskDescription.trim() === ''){
+        alert("Enter a task description.");
+        return;
+    }
+    if(taskDeadline.trim() === ''){
+        alert("Enter a task deadline.");
+        return;
+    }
+    
+}   
+var newTask = { name: taskName, description: taskDescription, deadline: taskDeadline};
+tasks.push(newTask);
+render();
+function render(){
+    taskTable.innerHTML= '';
+    tasks.forEach(function(task){
+        var row = document.createElement('tr');
+
+        const nameCell = document.createElement('td');
+        nameCell.textContent = task.name;
+        row.appendChild(nameCell);
+
+        const descriptionCell = document.createElement('td');
+        descriptionCell.textContent = task.description;
+        row.appendChild(descriptionCell);
+
+        const deadlineCell = document.createElement('td');
+        deadlineCell.textContent = task.deadline;
+        row.appendChild(deadlineCell);
+
+        taskTable.appendChild(row);
+    });
+
+
+    }
+
+    function init(){
+        render();
+
+    }
+    taskForm.addEventListener("submit", handleSubmission);
+    init();
